ea8621a5e8a5cb407f72f11cbdbbb5840487fa4b
ydh6226 <ydh6226@naver.com>
2021-07-12 03:09:31 +0900

    유틸리티객체, 날짜

--- src/main/java/com/thymeleaf/basic/BasicController.java
+++ src/main/java/com/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +28,12 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring!!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @GetMapping("/variable")
