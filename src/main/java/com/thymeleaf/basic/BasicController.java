b0dd47bde68d5ab5cdbafe41dee517518d143071
ydh6226 <ydh6226@naver.com>
2021-07-15 23:22:38 +0900

    URL 경로

--- src/main/java/com/thymeleaf/basic/BasicController.java
+++ src/main/java/com/thymeleaf/basic/BasicController.java
@@ -36,6 +36,13 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @GetMapping("/variable")
     public String variable(Model model) {
         User userA = new User("userA", 10);
